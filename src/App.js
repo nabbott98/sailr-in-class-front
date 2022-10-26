0eedc35807aa36416ecc7e554e4eb43b45cace43
nabbott98 <nicholasabbott98@gmail.com>
2022-10-25 20:47:35 -0400

    app.js updated

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './components/auth/SignUp'
 import SignIn from './components/auth/SignIn'
 import SignOut from './components/auth/SignOut'
 import ChangePassword from './components/auth/ChangePassword'
+import SailboatCreate from './components/SailboatCreate'
 
 const App = () => {
 
@@ -66,6 +67,13 @@ const App = () => {
             element={
               <RequireAuth user={user}>
                 <ChangePassword msgAlert={msgAlert} user={user} />
+              </RequireAuth>}
+          />
+		  <Route
+            path='/sailboats'
+            element={
+              <RequireAuth user={user}>
+                <SailboatCreate msgAlert={msgAlert} user={user} />
               </RequireAuth>}
           />
 				</Routes>
