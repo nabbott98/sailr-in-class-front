b45bed924f5d5d4e1148e1da7abb6babac30bb26
nabbott98 <nicholasabbott98@gmail.com>
2022-10-26 14:43:14 -0400

    api/sailboat.js

--- src/api/sailboat.js
+++ src/api/sailboat.js
@@ -13,3 +13,46 @@ export const sailboatCreate = (data, user) => {
 		},
 	})
 }
+
+export const sailboatIndex = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/sailboats',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+export const sailboatShow = (user, id) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/sailboats/' + id,
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+export const sailboatUpdate = (data, user, id) => {
+	return axios({
+		method: 'PATCH',
+		url: apiUrl + '/sailboats/' + id,
+		data: {
+			sailboat: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+export const sailboatDelete = (user, id) => {
+	return axios({
+		method: 'DELETE',
+		url: apiUrl + '/sailboats/' + id,
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
